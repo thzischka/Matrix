ac9347dcdc2bdb17e2ffe57e6b6aec68351fc1e9
Andrew Eeckman <andreweeckman@campus-030-116.ucdavis.edu>
2018-03-05 13:57:55 -0800

    Changed the standard from 11 to 14

---
+++ cmake-build-debug/CMakeFiles/matOperations.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matOperations.dir/main.cpp.o"
+  "matOperations.pdb"
+  "matOperations"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matOperations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/matOperations.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for matOperations.
+# This may be replaced when dependencies are built.
